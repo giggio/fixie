8c07eae79b87c279e409b82293488c88e7942afe
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-31 18:28:30 -0500

    The TestExplorerListener reports test discovery and execution results by writing JSON messages to a given Stream. It does so with a BinaryWriter so that each distinct JSON message can be unambiguously written and so that a consumer of those messages can unambiguously read them back from the Stream.

---
+++ src/Fixie.Tests/Execution/Listeners/TestExplorerListenerTests.cs
@@ -0,0 +1,157 @@
+﻿namespace Fixie.Tests.Execution.Listeners
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.Serialization.Json;
+    using System.Text;
+    using Assertions;
+    using Fixie.Execution;
+    using Fixie.Execution.Listeners;
+
+    public class TestExplorerListenerTests : MessagingTests
+    {
+        public void ShouldReportDiscoveredMethodsAsStreamOfJsonMessages()
+        {
+            var results = new List<TestExplorerListener.Test>();
+
+            using (var stream = new MemoryStream())
+            using (var reader = new BinaryReader(stream))
+            {
+                var listener = new TestExplorerListener(stream);
+
+                using (var console = new RedirectedConsole())
+                {
+                    Discover(listener);
+
+                    console.Lines().ShouldBeEmpty();
+                }
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                while (stream.Position != stream.Length)
+                    results.Add(Deserialize<TestExplorerListener.Test>(reader.ReadString()));
+
+                results = results.OrderBy(x => x.FullyQualifiedName).ToList();
+            }
+
+            results.Count.ShouldEqual(5);
+
+            var fail = results[0];
+            var failByAssertion = results[1];
+            var pass = results[2];
+            var skipWithoutReason = results[3];
+            var skipWithReason = results[4];
+
+            fail.FullyQualifiedName.ShouldEqual(TestClass + ".Fail");
+            fail.DisplayName.ShouldEqual(TestClass + ".Fail");
+
+            failByAssertion.FullyQualifiedName.ShouldEqual(TestClass + ".FailByAssertion");
+            failByAssertion.DisplayName.ShouldEqual(TestClass + ".FailByAssertion");
+
+            pass.FullyQualifiedName.ShouldEqual(TestClass + ".Pass");
+            pass.DisplayName.ShouldEqual(TestClass + ".Pass");
+
+            skipWithoutReason.FullyQualifiedName.ShouldEqual(TestClass + ".SkipWithoutReason");
+            skipWithoutReason.DisplayName.ShouldEqual(TestClass + ".SkipWithoutReason");
+
+            skipWithReason.FullyQualifiedName.ShouldEqual(TestClass + ".SkipWithReason");
+            skipWithReason.DisplayName.ShouldEqual(TestClass + ".SkipWithReason");
+        }
+
+        public void ShouldReportResultsAsStreamOfJsonMessages()
+        {
+            var results = new List<TestExplorerListener.TestResult>();
+
+            using (var stream = new MemoryStream())
+            using (var reader = new BinaryReader(stream))
+            {
+                var listener = new TestExplorerListener(stream);
+
+                using (var console = new RedirectedConsole())
+                {
+                    Run(listener);
+
+                    console.Lines()
+                        .ShouldEqual(
+                            "Console.Out: Fail",
+                            "Console.Error: Fail",
+                            "Console.Out: FailByAssertion",
+                            "Console.Error: FailByAssertion",
+                            "Console.Out: Pass",
+                            "Console.Error: Pass");
+                }
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                while (stream.Position != stream.Length)
+                    results.Add(Deserialize<TestExplorerListener.TestResult>(reader.ReadString()));
+            }
+
+            results.Count.ShouldEqual(5);
+
+            var skipWithReason = results[0];
+            var skipWithoutReason = results[1];
+            var fail = results[2];
+            var failByAssertion = results[3];
+            var pass = results[4];
+
+            skipWithReason.FullyQualifiedName.ShouldEqual(TestClass + ".SkipWithReason");
+            skipWithReason.DisplayName.ShouldEqual(TestClass + ".SkipWithReason");
+            skipWithReason.Outcome.ShouldEqual("Skipped");
+            skipWithReason.Duration.ShouldEqual(TimeSpan.Zero);
+            skipWithReason.ErrorMessage.ShouldEqual("Skipped with reason.");
+            skipWithReason.ErrorStackTrace.ShouldBeNull();
+            skipWithReason.Output.ShouldBeNull();
+
+            skipWithoutReason.FullyQualifiedName.ShouldEqual(TestClass + ".SkipWithoutReason");
+            skipWithoutReason.DisplayName.ShouldEqual(TestClass + ".SkipWithoutReason");
+            skipWithoutReason.Outcome.ShouldEqual("Skipped");
+            skipWithoutReason.Duration.ShouldEqual(TimeSpan.Zero);
+            skipWithoutReason.ErrorMessage.ShouldBeNull();
+            skipWithoutReason.ErrorStackTrace.ShouldBeNull();
+            skipWithoutReason.Output.ShouldBeNull();
+
+            fail.FullyQualifiedName.ShouldEqual(TestClass + ".Fail");
+            fail.DisplayName.ShouldEqual(TestClass + ".Fail");
+            fail.Outcome.ShouldEqual("Failed");
+            fail.Duration.ShouldBeGreaterThanOrEqualTo(TimeSpan.Zero);
+            fail.ErrorMessage.ShouldEqual("'Fail' failed!");
+            fail.ErrorStackTrace
+                .CleanStackTraceLineNumbers()
+                .Lines()
+                .ShouldEqual("Fixie.Tests.FailureException", At("Fail()"));
+            fail.Output.Lines().ShouldEqual("Console.Out: Fail", "Console.Error: Fail");
+
+            failByAssertion.FullyQualifiedName.ShouldEqual(TestClass + ".FailByAssertion");
+            failByAssertion.DisplayName.ShouldEqual(TestClass + ".FailByAssertion");
+            failByAssertion.Outcome.ShouldEqual("Failed");
+            failByAssertion.Duration.ShouldBeGreaterThanOrEqualTo(TimeSpan.Zero);
+            failByAssertion.ErrorMessage.Lines().ShouldEqual(
+                "Assertion Failure",
+                "Expected: 2",
+                "Actual:   1");
+            failByAssertion.ErrorStackTrace
+                .CleanStackTraceLineNumbers()
+                .ShouldEqual(At("FailByAssertion()"));
+            failByAssertion.Output.Lines().ShouldEqual("Console.Out: FailByAssertion", "Console.Error: FailByAssertion");
+
+            pass.FullyQualifiedName.ShouldEqual(TestClass + ".Pass");
+            pass.DisplayName.ShouldEqual(TestClass + ".Pass");
+            pass.Outcome.ShouldEqual("Passed");
+            pass.Duration.ShouldBeGreaterThanOrEqualTo(TimeSpan.Zero);
+            pass.ErrorMessage.ShouldBeNull();
+            pass.ErrorStackTrace.ShouldBeNull();
+            pass.Output.Lines().ShouldEqual("Console.Out: Pass", "Console.Error: Pass");
+        }
+
+        static T Deserialize<T>(string content)
+        {
+            var deserializer = new DataContractJsonSerializer(typeof(T));
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+                return (T)deserializer.ReadObject(stream);
+        }
+    }
+}
---
+++ src/Fixie/Execution/Listeners/TestExplorerListener.cs
@@ -0,0 +1,115 @@
+﻿namespace Fixie.Execution.Listeners
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.Serialization.Json;
+    using System.Text;
+    using Execution;
+
+    public class TestExplorerListener :
+        Handler<MethodDiscovered>,
+        Handler<CaseSkipped>,
+        Handler<CasePassed>,
+        Handler<CaseFailed>
+    {
+        readonly BinaryWriter writer;
+
+        public TestExplorerListener(Stream outputStream)
+        {
+            writer = new BinaryWriter(outputStream);
+        }
+
+        public void Handle(MethodDiscovered message)
+        {
+            var methodGroup = new MethodGroup(message.Class, message.Method);
+
+            Write(new Test
+            {
+                FullyQualifiedName = methodGroup.FullName,
+                DisplayName = methodGroup.FullName
+            });
+        }
+
+        public void Handle(CaseSkipped message)
+        {
+            Write(message, x =>
+            {
+                x.Outcome = "Skipped";
+                x.ErrorMessage = message.Reason;
+            });
+        }
+
+        public void Handle(CasePassed message)
+        {
+            Write(message, x =>
+            {
+                x.Outcome = "Passed";
+            });
+        }
+
+        public void Handle(CaseFailed message)
+        {
+            var exception = message.Exception;
+
+            Write(message, x =>
+            {
+                x.Outcome = "Failed";
+                x.ErrorMessage = exception.Message;
+                x.ErrorStackTrace = exception.TypedStackTrace();
+            });
+        }
+
+        void Write(CaseCompleted message, Action<TestResult> customize)
+        {
+            var testResult = new TestResult
+            {
+                FullyQualifiedName = FullyQualifiedName(message.Class, message.Method),
+                DisplayName = message.Name,
+                Duration = message.Duration,
+                Output = message.Output
+            };
+
+            customize(testResult);
+
+            Write(testResult);
+        }
+
+        static string FullyQualifiedName(Type @class, MethodInfo method)
+            => new MethodGroup(@class, method).FullName;
+
+        void Write<T>(T message)
+        {
+            writer.Write(Serialize(message));
+            writer.Flush();
+        }
+
+        static string Serialize<T>(T message)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, message);
+                return Encoding.UTF8.GetString(stream.ToArray());
+            }
+        }
+
+        public class Test
+        {
+            public string FullyQualifiedName { get; set; }
+            public string DisplayName { get; set; }
+        }
+
+        public class TestResult
+        {
+            public string FullyQualifiedName { get; set; }
+            public string DisplayName { get; set; }
+            public string Outcome { get; set; }
+            public TimeSpan Duration { get; set; }
+            public string Output { get; set; }
+            public string ErrorMessage { get; set; }
+            public string ErrorStackTrace { get; set; }
+        }
+    }
+}
