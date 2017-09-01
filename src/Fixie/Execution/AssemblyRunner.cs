bbac97e4ef2debd9c68387117022db03b950001e
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-31 19:25:09 -0500

    AssemblyRunner.Main reads a command from FIXIE_NAMED_PIPE, when present.

--- src/Fixie/Execution/AssemblyRunner.cs
+++ src/Fixie/Execution/AssemblyRunner.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.IO;
+    using System.IO.Pipes;
     using System.Reflection;
     using Cli;
 
     public class AssemblyRunner
     {
+        const int Success = 0;
         const int FatalError = -1;
 
         public static int Main(string[] arguments)
@@ -20,8 +22,29 @@
 
                 var assemblyDirectory = Path.GetDirectoryName(assemblyFullPath);
 
+                var command = "Run Assembly";
+
+                var namedPipe = Environment.GetEnvironmentVariable("FIXIE_NAMED_PIPE");
+                if (namedPipe != null)
+                {
+                    using (var pipeClient = new NamedPipeClientStream(namedPipe))
+                    using (var reader = new BinaryReader(pipeClient))
+                        command = reader.ReadString();
+                }
+
                 using (var executionProxy = new ExecutionProxy(assemblyDirectory))
+                {
+                    if (command == "Discover Methods")
+                    {
+                        executionProxy.DiscoverMethods(assemblyFullPath, arguments);
+                        return Success;
+                    }
+
+                    if (command == "Run Methods")
+                        return executionProxy.RunMethods(assemblyFullPath, arguments, new string[] { }/*methods from command*/);
+
                     return executionProxy.RunAssembly(assemblyFullPath, arguments);
+                }
             }
             catch (Exception exception)
             {
