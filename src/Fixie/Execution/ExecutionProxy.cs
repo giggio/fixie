f07ee6915b604362da777e8407726fb8ef214083
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-31 19:24:48 -0500

    ExecutionProxy enlists TestExplorerListener when FIXIE_NAMED_PIPE is defined.

--- src/Fixie/Execution/ExecutionProxy.cs
+++ src/Fixie/Execution/ExecutionProxy.cs
@@ -96,7 +96,11 @@
 
         IEnumerable<Listener> DefaultExecutionListeners(Options options)
         {
-            if (ShouldUseTeamCityListener(options))
+            if (ShouldUseTestExplorerListener())
+            {
+                yield return new TestExplorerListener(null);
+            }
+            else if (ShouldUseTeamCityListener(options))
                 yield return new TeamCityListener();
             else
                 yield return new ConsoleListener();
@@ -128,6 +132,11 @@
         static bool ShouldUseAppVeyorListener()
         {
             return Environment.GetEnvironmentVariable("APPVEYOR") == "True";
+        }
+
+        static bool ShouldUseTestExplorerListener()
+        {
+            return Environment.GetEnvironmentVariable("FIXIE_NAMED_PIPE") != null;
         }
     }
 }
